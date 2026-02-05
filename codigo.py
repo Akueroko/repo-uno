3eb49d11f53f80dc7bc65601d9c1a869f7cce77d
Alejo <alessiomiraville@gmail.com>
2026-02-05 21:58:59 +0100

    agregando el nombre2

--- codigo.py
+++ codigo.py
@@ -1,7 +1,8 @@
 #Esto fue desarrollado por Akuru
 nombre = "Sono"
+nombre2 = "Akuru"
 apellido = "Aleio"
 
-saludo = "Hola " + nombre + " " + apellido + " Como estas?"
+saludo = "Hola " + nombre + nombre2 + " " + apellido + " Como estas?"
 
 print(saludo)
